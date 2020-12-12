833d107e78f9826779ac24a8267da2dae3d95819
mamat5592 <75572925+mamat5592@users.noreply.github.com>
2020-12-12 17:55:26 +0330

    metronem bug fixed

--- src/components/metronome/index.js
+++ src/components/metronome/index.js
@@ -16,7 +16,7 @@ class MComponent extends React.Component{
         this.pauseAudio = this.pauseAudio.bind(this);
     }
 
-    componentDidUpdate(prevState) {
+    componentDidUpdate(prevProps,prevState) {
         if(this.state.beat !== prevState.beat){
             clearInterval(this.intervalID);
         }
